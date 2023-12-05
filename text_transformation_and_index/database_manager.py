8e573ba32f18b3a17428dbce3398fdf6737a3d5a
didikid3 <didikid3@gmail.com>
2023-12-05 14:17:56 -0800

    Feat: Stop Word and Stemming

---
+++ text_transformation_and_index/database_manager.py
@@ -0,0 +1,27 @@
+from pymongo import MongoClient
+
+def connectDatabase():
+    DB_NAME = "cs4250project"
+    DB_HOST = "localhost"
+    DB_PORT = 27017
+
+    try:
+        client = MongoClient(host=DB_HOST, port=DB_PORT)
+        db = client[DB_NAME]
+        return db
+    except:
+        print("Database not connected succesfully.")
+
+
+def get_professor_research_blob(col):
+    return col.find({},{\
+        "_id": 0,
+        "name": 1,
+        "research": 1,
+    })
+    
+def insertDocument(col, key, document):
+    col.update_one(key, document, upsert=True)
+
+def update_index(col, term, professorName, frequency):
+    pass
---
+++ text_transformation_and_index/research_indexing.py
@@ -0,0 +1,39 @@
+import database_manager
+import indexing_utils
+
+def getProfessorData(databaseHandler):
+    return database_manager.get_professor_research_blob(databaseHandler)
+
+def indexData(databaseHandler, professorData):
+    professorNames, researchBlobs = [], []
+    for professor in professorData:
+        professorNames.append(professor['name'])
+        researchBlobs.append(professor['research'])
+
+    # Perform Text Transformation
+    researchBlobs.append("Trees Tree")
+    researchBlobs = indexing_utils.remove_stop_words(researchBlobs)
+    vocab, vector_array, researchBlobs = indexing_utils.stemming(researchBlobs)
+
+    # Find all Indexes
+    # Return a Dictionary of All The Terms {'Term': Term Frequency}
+    tokens = indexing_utils.index(databaseHandler, professorNames, vocab, vector_array)
+
+    return 1
+
+def main():
+    print("Running indexing...")
+    database = database_manager.connectDatabase()
+
+    professor_data = getProfessorData(database.professors)
+
+    # for prof in professor_data:
+    #     print(prof['name'])
+    #     print(prof['research'], end="\n\n")
+    res = indexData(database.index, professor_data)
+
+    if res == 1:
+        print("Finished Successfully")
+
+if __name__ == "__main__":
+    main()
